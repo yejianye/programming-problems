b75e9e8b053e36d69a07eca13e24e57c68d625fe
Ryan Ye <yejianye@gmail.com>
2012-07-09 11:45:58 +0800

    Python's internal support for big numbers is very efficient, so there's no need to reinvent the wheel

--- spoj/ARITH/solution.py
+++ spoj/ARITH/solution.py
@@ -5,84 +5,16 @@ from pprint import pprint
 
 re_expr = re.compile(r'([0-9]+)([*+-])([0-9]+)')
 def add(n1, n2):
-	if len(n1) > len(n2):
-		n2 = n2.zfill(len(n1))
-	else:
-		n1 = n1.zfill(len(n2))
-	carry = 0
-	result = []
-	for x, y in itertools.izip(reversed(n1), reversed(n2)):
-		z = int(x) + int(y) + carry
-		if z > 9:
-			z, carry = z - 10, 1
-		else:
-			carry = 0
-		result.append(z)
-	if carry == 1:
-		result.append(1)
-	return ''.join(str(x) for x in reversed(result))
+	return str(long(n1) + long(n2))
 
 def substract(n1, n2):
-	carry = 0
-	result = []
-	for x, y in itertools.izip(reversed(n1), reversed(n2)):
-		z = int(x) - int(y) - carry
-		if z < 0:
-			z, carry = z + 10, 1
-		else:
-			carry = 0
-		result.append(z)
-	if carry:
-		result = decrement(n1[:(len(n1) - len(n2))]) + ''.join(str(x) for x in reversed(result))
-	else:
-		result = n1[:(len(n1) - len(n2))] + ''.join(str(x) for x in reversed(result))
-	return result.lstrip('0')
-
-def decrement(x):
-	for idx, val in enumerate(reversed(x)):
-		if val > '0':
-			break
-	if idx == len(x) - 1 and val == '1':
-		return '9' * idx
-	else:
-		return x[:-(idx+1)] + str(int(val) - 1) + '9' * idx
+	return str(long(n1) - long(n2))
 
 def mul(n1, n2):
-	mul_pass_caches.clear()
-	intermedia = [mul_pass(n1, int(x)) for x in reversed(n2)]
-	return mul_sum(intermedia, len(n1)+1), [''.join(str(x) for x in reversed(one_pass)) for one_pass in intermedia]
-
-mul_pass_caches = {}
-def mul_pass(n, m):
-	if not mul_pass_caches.has_key(m):
-		if m == 0:
-			mul_pass_caches[m] = [0] 
-		else:
-			carry = 0
-			result = []
-			for x in reversed(n):
-				y = int(x) * m + carry
-				carry, z = y / 10, y % 10
-				result.append(z)
-			if carry:
-				result.append(carry)
-			mul_pass_caches[m] = result 
-	return mul_pass_caches[m]
-
-def mul_sum(intermedia, max_length):
-	total_length = len(intermedia) - 1 + len(intermedia[-1])
-	carry = 0
-	result = []
-	for i in xrange(total_length):
-		y = carry
-		for j in xrange(max(0, i - max_length), min(i+1, len(intermedia))):
-			if i - j < len(intermedia[j]):
-				y += intermedia[j][i-j]
-		carry = y / 10
-		result.append(y % 10)
-	if carry:
-		result.append(carry)
-	return ''.join(str(x) for x in reversed(result))
+	n1_int = long(n1)
+	n2_int = long(n2)
+	intermedia = [n1_int * int(x) for x in reversed(n2)]
+	return str(n1_int * n2_int), map(str, intermedia)
 
 def calc(expr):
 	first, op, second = re_expr.match(expr).groups()
