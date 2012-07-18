755c7708201978d5aed9d52b0e561118c53c6dcb
Ryan Ye <yejianye@gmail.com>
2012-07-18 21:20:17 +0800

    solve ACODE

--- spoj/ACODE/solution.py
+++ spoj/ACODE/solution.py
@@ -5,16 +5,13 @@ def acode(data):
 	for i in xrange(len(data) - 1, -1, -1):
 		# corner cases
 		if data[i] == '0':
-			continue
-		if i == len(data) - 1:
-			continue
-		# normal cases
-		if data[i] >= '3':
+			result[i] = 0
+		elif i == len(data) - 1:
+			result[i] = 1
+		elif data[i] >= '3':
 			result[i] = result[i+1]
 		elif data[i] == '2' and data[i+1] > '6':
 			result[i] = result[i+1]
-		elif data[i+1] == '0':
-			result[i] = result[i+2]
 		else:
 			result[i] = result[i+1] + result[i+2]
 	return result[0]
